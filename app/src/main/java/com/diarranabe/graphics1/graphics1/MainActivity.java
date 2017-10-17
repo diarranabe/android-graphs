442d47115d43ed16a790734568bb7f0f3068a44e
Maud <maudmcok001@gmail.com>
2017-10-17 09:09:23 +0200

    comite 003

--- app/src/main/java/com/diarranabe/graphics1/graphics1/MainActivity.java
+++ app/src/main/java/com/diarranabe/graphics1/graphics1/MainActivity.java
@@ -16,7 +16,7 @@ import android.widget.ImageView;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+public class MainActivity extends AppCompatActivity {
     ImageView zzst;
     Bitmap bitmap;
     Paint paint;
@@ -50,6 +50,40 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         zzst = (ImageView) findViewById(R.id.imageView);
 
+
+        zzst.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        downx = event.getX();
+                        downy = event.getY();
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        upx = event.getX();
+                        upy = event.getY();
+                        if( (Math.abs(upx - downx)< 10) ||  (Math.abs(upy - downy)< 10)) {
+
+                            Log.d("XXXX", "nodeH => " +upx + " nodeW => " + upy + " nodeName touch");
+                            canvas.drawCircle(upx, upy, 60, paint);
+                        } else {
+
+
+                        }
+                        zzst.invalidate();
+                        break;
+                    case MotionEvent.ACTION_CANCEL:
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
         Display currentDisplay = getWindowManager().getDefaultDisplay();
 
         Log.d("XXXX", "imgH => " + currentDisplay.getWidth() + " imgW => " + currentDisplay.getHeight());
@@ -65,7 +99,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         }
         zzst.setImageBitmap(bitmap);
 
-        zzst.setOnTouchListener(this);
+       // zzst.setOnTouchListener(this);
 
         List<Node> nodes = graph.getNoeuds();
         for (Node node : nodes) {
@@ -77,34 +111,34 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void initialize() {
-        Node n1 = new Node(50, 50, " N1 ", Color.BLACK);
+      /*  Node n1 = new Node(50, 50, " N1 ", Color.BLACK);
 
         Node n2 = new Node(50, 50, " N2 ", Color.BLACK );
         Node n3 = new Node(50, 110, " N3 ",  Color.BLACK);
         Node n4 = new Node(50, 200, " N4 ",  Color.BLACK);
-        Node n5 = new Node(120, 50, " N5 ",  Color.BLACK);
+        Node n5 = new Node(120, 50, " N5 ",  Color.BLACK);*/
         Node n6 = new Node(120, 110, " N6 ",  Color.BLACK);
         Node n7 = new Node(120, 200, " N7 ",  Color.BLACK);
         Node n8 = new Node(250, 250, " N8 ",  Color.BLACK);
         Node n9 = new Node(500, 500, " N9 ",  Color.BLACK);
-        graph.addNode(n1);
+      /*  graph.addNode(n1);
         graph.addNode(n2);
         graph.addNode(n3);
         graph.addNode(n4);
-        graph.addNode(n5);
+        graph.addNode(n5);*/
         graph.addNode(n6);
         graph.addNode(n7);
         graph.addNode(n8);
         graph.addNode(n9);
 
-        Arc a1 = new Arc(n1, n3);
+       /* Arc a1 = new Arc(n1, n3);
         graph.addArc(a1);
         Arc a2 = new Arc(n1, n2);
         graph.addArc(a2);
         Arc a3 = new Arc(n2, n4);
         graph.addArc(a3);
         Arc a4 = new Arc(n3, n5);
-        graph.addArc(a4);
+        graph.addArc(a4);*/
 
 
     }
@@ -118,7 +152,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         zzst.invalidate();
     }
 
-
+/*
     @Override
     public boolean onTouch(View view, MotionEvent event) {
         int action = event.getAction();
@@ -148,5 +182,5 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         }
 
         return true;
-    }
+    }*/
 }
