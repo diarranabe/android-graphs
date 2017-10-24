c4db13737f4c3cde172b806f86f251646de26560
Maud <maudmcok001@gmail.com>
2017-10-24 08:58:58 +0200

    comite 006 touck to draw modif to new classe ok touch exist to delete put in coment ok

--- app/src/main/java/com/diarranabe/graphics1/graphics1/DrawableGraph.java
+++ app/src/main/java/com/diarranabe/graphics1/graphics1/DrawableGraph.java
@@ -1,92 +1,169 @@
 package com.diarranabe.graphics1.graphics1;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 
 /**
- * Created by matok on 11/10/2017.
+ * Created by matok on 24/10/2017.
  */
 
-public class DrawableGraph extends View {
-    public DrawableGraph(Context context) {
-        super(context);
+public class DrawableGraph extends View implements View.OnTouchListener {
+
+
+    Paint paint, painte;
+    Canvas canvas;
+
+    Boolean supOne = false;
+
+
+    int downx = 50;
+    int downy = 50;
+
+    int upx = 0;
+    int upy = 0;
+
+    int umpx = 0;
+    int umpy = 0;
+
+    static {
+        graph = new Graph(5);
+
     }
 
-    private Paint paint;
-    int x = 50;
-    int y = 50;
+    private static Graph graph;
 
-    Canvas canvas = new Canvas() ;
+    public DrawableGraph(Context context) {
+        super(context);
+        this.setOnTouchListener(this);
 
-
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        // TODO Auto-generated method stub
         super.onDraw(canvas);
-        this.canvas = canvas ;
-       /* int radius;
-        radius = 25;
-        Paint paint = new Paint();
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.WHITE);
-        canvas.drawPaint(paint);
-        // Use Color.parseColor to define HTML colors
-        paint.setColor(Color.parseColor("#CD5C5C"));
-        canvas.drawCircle(x , y , radius, paint);*/
+        this.canvas =  canvas ;
+
+        paint = new Paint();
+        painte = new Paint();
+        paint.setColor(Color.DKGRAY);
+        painte.setColor(Color.WHITE);
 
     }
 
-    public void kamiDraw(Graph graph){
-        List<Node> nodes = graph.getNoeuds();
-        List<Arc> arcs = graph.getArcs() ;
-        for (Node node: nodes ) {
+
+    public void initialize() {
+
+        int ic = 0;
+        for (Node node : graph.getNoeuds()) {
             drawNode(node);
+            ic++;
         }
-        for (Arc arc : arcs ) {
-                 drawArc(arc);
+        if (ic == 0) {
+            canvas.drawCircle(0, 0, 1500, painte);
+            Log.e("XXXX", " ===> Winter " + ic);
         }
+        Log.e("XXXX", " ===> ic =  " + ic);
 
 
     }
 
-    private void drawArc(Arc arc){
 
-
+    public void drawNode(Node node) {
+        Log.d("XXXX", "nodeH => " + node.getX() + " nodeW => " + node.getY() + " nodeName => " + node.getEtiquete());
+        canvas.drawCircle(node.getX(), node.getY(), node.getDiameter(), paint);
+        this.invalidate();
     }
-    private void drawNode(Node node){
-        int radius;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            radius = node.getDiameter();
-        }else {
-            radius =75;
+
+    public void unDrawNode(Node node)  {
+        canvas.drawCircle(node.getX(), node.getY(), node.getDiameter() + 3, painte);
+    }
+
+
+
+    public void drawArc(int ax,int ay, int bx,int by){
+
+        Paint p = new Paint();
+        // smooths
+        p.setAntiAlias(true);
+        p.setColor(Color.RED);
+        p.setStyle(Paint.Style.STROKE);
+        p.setStrokeWidth(5);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            canvas.drawArc(ax,ay,bx,by, 90, 45, true, p);
         }
-
-        radius = 25;
-        Paint paint = new Paint();
-        paint.setStyle(Paint.Style.FILL);
-        paint.setColor(Color.WHITE);
-        canvas.drawPaint(paint);
-        // Use Color.parseColor to define HTML colors
-        paint.setColor(Color.parseColor("#CD5C5C"));
-        canvas.drawCircle(x , y , radius, paint);
-
-
-        Log.d("XXXX"," Info  => drawNode +> " + node.getEtiquete());
-       // canvas.drawPaint(paint);
-        // Use Color.parseColor to define HTML colors
-        //paint.setColor(Color.BLACK);
-       // canvas.drawCircle( ,  , radius, paint);
+        this.invalidate();
 
     }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+
+
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+
+                downx = (int) event.getX();
+                downy = (int) event.getY();
+                Log.e("XXXX", " ===> Cool touch " );
+                Node auxNode = graph.selectedNode(downx, downy);
+                if (null != auxNode) {
+
+                         /*  unDrawNode(auxNode);
+                            graph.removeNode(auxNode);
+                            initialize();*/
+                    supOne = true;
+                }
+
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                umpx = (int) event.getX();
+                umpy = (int) event.getY();
+               /* if (supOne){
+                    drawArc(downx,downy,umpx,umpy);
+                    Log.e("XXXX", " ===> draw arc =  " );
+                }*/
+                        /* umpx = (int) event.getX();
+                        umpy = (int) event.getY();
+
+                        Node auxNode = graph.selectedNode(downx, downy);
+                        if (auxNode != null)
+                            unDrawNode(auxNode);
+                        auxNode.setX(umpx);
+                        auxNode.setY(umpy);
+                        drawNode(auxNode);
+                        zzst.invalidate();*/
+
+
+                break;
+            case MotionEvent.ACTION_UP:
+                upx = (int) event.getX();
+                upy = (int) event.getY();
+                if (!supOne) {
+                    if ((Math.abs(upx - downx) < 10) && (Math.abs(upy - downy) < 10)) {
+                        if (graph.addNode(upx, upy)) initialize();
+                    }
+                } else {
+                    supOne = false;
+                }
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
 }
--- app/src/main/java/com/diarranabe/graphics1/graphics1/MainActivity.java
+++ app/src/main/java/com/diarranabe/graphics1/graphics1/MainActivity.java
@@ -1,162 +1,20 @@
 package com.diarranabe.graphics1.graphics1;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.Display;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView zzst;
-    Bitmap bitmap;
-    Paint paint, painte;
-    Canvas canvas;
 
-    Boolean supOne = false;
-
-
-    int downx = 50;
-    int downy = 50;
-
-    int upx = 0;
-    int upy = 0;
-
-    int umpx = 0;
-    int umpy = 0;
-
-    private static Graph graph;
-
-    static {
-        graph = new Graph(5);
-    }
+    DrawableGraph myDraw ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        //   DrawableGraph mygraph = new DrawableGraph(this) ;
-
-        zzst = new ImageView(this);
-
-        // mygraph.kamiDraw(graph);
-        setContentView(R.layout.activity_main);
-
-
-        // champs = (ImageView) findViewById(R.id.imageView);
-
-
-        zzst = (ImageView) findViewById(R.id.imageView);
-
-
-        Display currentDisplay = getWindowManager().getDefaultDisplay();
-
-        Log.d("XXXX", "imgH pic => " + zzst.getWidth() + " imgW => " + zzst.getWidth());
-        Log.d("XXXX", "imgH => " + currentDisplay.getWidth() + " imgW => " + currentDisplay.getHeight());
-        bitmap = Bitmap.createBitmap(currentDisplay.getWidth(), currentDisplay.getHeight(),
-                Bitmap.Config.ARGB_8888);
-        canvas = new Canvas(bitmap);
-        canvas.save();
-        paint = new Paint();
-        painte = new Paint();
-        painte.setColor(Color.WHITE);
-        paint.setColor(Color.DKGRAY);
-
-        zzst.setImageBitmap(bitmap);
-
-
-        initialize();
-        //  setContentView(this);
-
-        zzst.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                int action = event.getAction();
-                switch (action) {
-                    case MotionEvent.ACTION_DOWN:
-
-                        downx = (int) event.getX();
-                        downy = (int) event.getY();
-
-                        Node auxNode = graph.selectedNode(downx, downy);
-                        if (null != auxNode) {
-                           unDrawNode(auxNode);
-                            graph.removeNode(auxNode);
-                            initialize();
-                            supOne = true;
-                        }
-
-
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-
-                       /* umpx = (int) event.getX();
-                        umpy = (int) event.getY();
-
-                        Node auxNode = graph.selectedNode(downx, downy);
-                        if (auxNode != null)
-                            unDrawNode(auxNode);
-                        auxNode.setX(umpx);
-                        auxNode.setY(umpy);
-                        drawNode(auxNode);
-                        zzst.invalidate();*/
-
-
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        upx = (int) event.getX();
-                        upy = (int) event.getY();
-                        if (!supOne) {
-                            if ((Math.abs(upx - downx) < 10) && (Math.abs(upy - downy) < 10)) {
-                                if (graph.addNode(upx, upy)) initialize();
-                            }
-                        } else {
-                            supOne = false;
-                        }
-                        break;
-                    case MotionEvent.ACTION_CANCEL:
-                        break;
-                    default:
-                        break;
-                }
-                return true;
-            }
-        });
-
+        myDraw = new DrawableGraph(this);
+        setContentView(myDraw);
+      //  myDraw.initialize();
     }
 
-    public void initialize() {
-
-        int ic = 0;
-        for (Node node : graph.getNoeuds()) {
-            drawNode(node);
-            ic++;
-        }
-        if (ic == 0) {
-            canvas.drawCircle(0, 0, 1500, painte);
-            Log.e("XXXX", " ===> Winter " + ic);
-        }
-        Log.e("XXXX", " ===> ic =  " + ic);
-
-
-    }
-
-
-    public void drawNode(Node node) {
-        Log.d("XXXX", "nodeH => " + node.getX() + " nodeW => " + node.getY() + " nodeName => " + node.getEtiquete());
-        canvas.drawCircle(node.getX(), node.getY(), node.getDiameter(), paint);
-        zzst.invalidate();
-    }
-
-    public void unDrawNode(Node node)  {
-        canvas.drawCircle(node.getX(), node.getY(), node.getDiameter() + 3, painte);
-    }
 
 
 }
